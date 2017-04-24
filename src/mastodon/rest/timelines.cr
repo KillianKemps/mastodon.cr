4a7851eaf3315818baff8ecf8e092928b0583303
Decors <decors1215@gmail.com>
2017-04-24 12:50:15 +0900

    Add timeline API pagination
    * See https://github.com/tootsuite/documentation/commit/a9ab3729a0502e14ffe4134ea422b3e73cb39c12

--- src/mastodon/rest/timelines.cr
+++ src/mastodon/rest/timelines.cr
@@ -6,26 +6,32 @@ module Mastodon
     module Timelines
       TIMELINES_BASE = "/api/v1/timelines"
 
-      def timeline_home(limit = DEFAULT_STATUSES_LIMIT)
+      def timeline_home(max_id = nil, since_id = nil, limit = DEFAULT_STATUSES_LIMIT)
         params = HTTP::Params.build do |param|
+          param.add "max_id", "#{max_id}" unless max_id.nil?
+          param.add "since_id", "#{since_id}" unless since_id.nil?
           param.add "limit", "#{limit}" if limit != DEFAULT_STATUSES_LIMIT && limit <= 80
         end
         response = get("#{TIMELINES_BASE}/home", params)
         Array(Response::Status).from_json(response)
       end
 
-      def timeline_public(local = false, limit = DEFAULT_STATUSES_LIMIT)
+      def timeline_public(max_id = nil, since_id = nil, local = false, limit = DEFAULT_STATUSES_LIMIT)
         params = HTTP::Params.build do |param|
           param.add "local", "" if local
+          param.add "max_id", "#{max_id}" unless max_id.nil?
+          param.add "since_id", "#{since_id}" unless since_id.nil?
           param.add "limit", "#{limit}" if limit != DEFAULT_STATUSES_LIMIT && limit <= 80
         end
         response = get("#{TIMELINES_BASE}/public", params)
         Array(Response::Status).from_json(response)
       end
 
-      def timeline_tag(hashtag, local = false, limit = DEFAULT_STATUSES_LIMIT)
+      def timeline_tag(hashtag, max_id = nil, since_id = nil, local = false, limit = DEFAULT_STATUSES_LIMIT)
         params = HTTP::Params.build do |param|
           param.add "local", "" if local
+          param.add "max_id", "#{max_id}" unless max_id.nil?
+          param.add "since_id", "#{since_id}" unless since_id.nil?
           param.add "limit", "#{limit}" if limit != DEFAULT_STATUSES_LIMIT && limit <= 80
         end
         response = get("#{TIMELINES_BASE}/tag/#{hashtag}", params)
